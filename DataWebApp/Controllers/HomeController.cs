865b04f20601687dc91e1b1d0ecc52c4d2a7b40e
Noosher <rwurton@gmail.com>
2025-12-02 00:42:23 -0600

    Add Tech Stack page and analysis dropdown
    Introduces a new TechStack.cshtml view and corresponding HomeController action to present the platform's architecture and roadmap. Adds an 'Analysis' dropdown to the main navigation with links to daily and hourly analysis pages for ASP.NET, Power BI, and MatPlotLib. This improves discoverability of analytics features and provides a detailed overview of the tech stack for users.

--- DataWebApp/Controllers/HomeController.cs
+++ DataWebApp/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace DataWebApp.Controllers
             return View();
         }
 
+        public IActionResult TechStack()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- DataWebApp/obj/Debug/net10.0/DataWebApp.AssemblyInfo.cs
+++ DataWebApp/obj/Debug/net10.0/DataWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a8f2f41df3e3cbb03cbf7e32e49a6990294535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a0b45061b6257f4916b18d33677d7ef94900f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
